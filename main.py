0139d035f68fda5a7e6f13668bd7b5bc8fe68853
SRGTUBE <161336524+SRGTUBE@users.noreply.github.com>
2025-03-03 19:15:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ async def help(ctx):
                       "│ <a:set:1345374633666416725> `.setbalance <user> <amount>`\n"
                       "│ <a:slots:1345374871734980608> `.slots <amount> [X5 MONEY]`\n"
                       "│ <:gcoin:1345375137100464168> `.give <user> <amount>`\n"
-                      "│ 🎲 `.dice <amount> <1-6> [X6 MONEY]`\n"
+                      "│ <a:dice:1345375794490507274> `.dice <amount> <1-6> [X6 MONEY]`\n"
                       "╰───────────⟡", inline=False)
 
 
